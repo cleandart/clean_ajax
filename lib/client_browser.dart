518d5c134100ffb25b4e5dc0f4e3b7a84e190cff
Samuel Hapak <samuel.hapak@gmail.com>
2013-12-08 23:46:33 +0100

    Improve documentation.

--- lib/client_browser.dart
+++ lib/client_browser.dart
@@ -3,8 +3,7 @@
 // BSD-style license that can be found in the LICENSE file.
 
 /**
- * A library for server-client communication and interaction
- * Client(Browser) side
+ * Browser implementation of [Connection].
  */
 library clean_ajax.client_browser;
 
@@ -12,6 +11,12 @@ import "dart:html";
 import 'client.dart';
 export 'client.dart';
 
+/**
+ * Create new [Connection] based on ajax polling.
+ *
+ * Expects [MultiRequestHandler] listening server side on the [url].
+ * Polling interval can be configured by [delayBetweenRequests].
+ */
 Connection createHttpConnection(url, Duration delayBetweenRequests) =>
   new Connection.config(
       new HttpTransport(HttpRequest.request, url, delayBetweenRequests)
